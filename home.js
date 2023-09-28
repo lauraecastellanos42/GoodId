82a57c66cd248ab1497df0684512139a0e26831e
Laura Castellanos <lauraecastellanos.42@gmail.com>
2023-09-27 21:33:09 -0500

    archivos Iniciales de prueba

---
+++ home.js
@@ -0,0 +1,9 @@
+const DescribeImage = document.getElementById("DescribeImage");
+
+function showTextArea(){
+    if (DescribeImage.style.display === "none") {
+        DescribeImage.style.display = "block";
+      } else {
+        DescribeImage.style.display = "none";
+    }
+}
